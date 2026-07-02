433da0099ced0856178fda2bce7e23cdc707794e
Hernane Mufua <239749143+hernanemufua@users.noreply.github.com>
2026-07-02 09:40:40 +0100

    Ajuste na responsividade

--- js/script.js
+++ js/script.js
@@ -73,3 +73,21 @@ topButton.addEventListener("click",()=>{
     });
 
 });
+const menuToggle = document.getElementById("menu-toggle");
+const navbar = document.querySelector(".navbar");
+
+menuToggle.addEventListener("click", () => {
+
+    navbar.classList.toggle("active");
+
+    if(navbar.classList.contains("active")){
+
+        menuToggle.textContent = "✕";
+
+    }else{
+
+        menuToggle.textContent = "☰";
+
+    }
+
+});
