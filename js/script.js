454ab5e1e32af471abc6f15e096cd9581c971a37
Hernane Mufua <239749143+hernanemufua@users.noreply.github.com>
2026-07-02 10:39:45 +0100

    alt

--- js/script.js
+++ js/script.js
@@ -91,3 +91,26 @@ menuToggle.addEventListener("click", () => {
     }
 
 });
+const reveals = document.querySelectorAll(".reveal");
+
+function revealOnScroll(){
+
+    const windowHeight = window.innerHeight;
+
+    reveals.forEach(item=>{
+
+        const top = item.getBoundingClientRect().top;
+
+        if(top < windowHeight - 120){
+
+            item.classList.add("active");
+
+        }
+
+    });
+
+}
+
+window.addEventListener("scroll", revealOnScroll);
+
+revealOnScroll();
